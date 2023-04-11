58d71bf92c5d676f70026e8d706482f41eadf4a4
lnilss <nili22re@student.ju.se>
2023-04-12 01:04:08 +0200

    incompleted tasks can now be deleted
    added more comments as well as figured out how to remove the tasks from the task array...just not the completed tasks array. no localstorage just yet

--- todo.js
+++ todo.js
@@ -44,6 +44,7 @@ function addTaskHandler() {
 
 // function to complete a task
 function completeTaskHandler(event) {
+  //event as argument
   const taskItem = event.target.parentNode;
   const taskName = taskItem.querySelector("li").innerText;
   const taskIndex = tasks.indexOf(taskName);
@@ -59,8 +60,14 @@ function completeTaskHandler(event) {
 }
 
 // function to delete a task
-function deleteTaskHandler() {
-  //event as argument
+function deleteTaskHandler(event) {
+  const taskItem = event.target.parentNode; //becomes parent of button element
+  const taskName = taskItem.querySelector("li").innerText;
+  //strict inequality operator to determine if task should be removed
+  //based on the taskName variable. lines 69-70 adapted from chatgpt
+  //and i had a lot of trouble figuring out how to do this
+  tasks = tasks.filter((task) => task !== taskName); //filter removes taskName from tasks array
+  completedTasks = completedTasks.filter((task) => task !== taskName); // remove task from completedTasks array (work in progress)
   taskItem.remove();
   updateTasks();
 }
@@ -68,7 +75,8 @@ function deleteTaskHandler() {
 // function to update the task list and completed task list
 function updateTasks() {
   completedTasksElement.innerHTML = "";
-  completedTasks.forEach((task) => { //clears the current HTML element content
+  completedTasks.forEach((task) => {
+    //clears the current HTML element content
     //and loops it through the completedTasks array
     const completedTaskElement = document.createElement("li");
     completedTaskElement.innerText = task;
